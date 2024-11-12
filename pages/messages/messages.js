f1b8ab59581e0fd4a0cda0ec826179edcf68947c
Mark Xu <xujianqiao86@gmail.com>
2024-11-12 19:06:53 +0800

    add message database

--- pages/messages/messages.js
+++ pages/messages/messages.js
@@ -8,32 +8,17 @@ Page({
   },
 
   fetchMessages() {
-    wx.request({
-      url: 'https://your-backend.com/api/messages', // API 地址
-      method: 'GET',
-      success: (res) => {
-        if (res.statusCode === 200) {
-          this.setData({ messages: res.data });
-        } else {
-          wx.showToast({
-            title: "无法获取消息列表",
-            icon: "none"
-          });
-        }
-      },
-      fail: () => {
-        wx.showToast({
-          title: "请求失败",
-          icon: "none"
-        });
-      }
-    });
-  },
-
-  viewMessage(event) {
-    const messageId = event.currentTarget.dataset.id;
-    wx.navigateTo({
-      url: `/pages/messageDetail/messageDetail?id=${messageId}`
+    const db = wx.cloud.database();
+    db.collection('Messages').get().then(res => {
+      this.setData({
+        messages: res.data
+      });
+    }).catch(err => {
+      wx.showToast({
+        title: "无法获取消息列表",
+        icon: "none"
+      });
+      console.error('数据库查询失败', err);
     });
   }
 });
